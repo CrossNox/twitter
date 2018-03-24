2eb4e407f4ab4eb7d64ba292208b51b2ec0f0822
CrossNox <ijmermet@gmail.com>
2018-03-24 20:40:29 -0300

    fix

--- twitter.rb
+++ twitter.rb
@@ -57,7 +57,7 @@ def stream_keywords(keywords,rt)
 
 	stream_client.filter(track: keywords.join(",")) do |object|
 	  if object.is_a?(Twitter::Tweet) && (rt ? true : !object.retweet?)
-		save_tweet object,"" unless Tweet.exists? twitter_id: tweet.id
+		save_tweet object,"" unless Tweet.exists? twitter_id: object.id
 	  end
 	end
 end
